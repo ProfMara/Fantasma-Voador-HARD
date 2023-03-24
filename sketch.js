a4551a2eec867cbafc7335062c819a369cd8cfa7
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-03-24 08:26:51 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,20 +8,7 @@ var somAssustador;
 var playerparado;
 
 function preload() {
-    //carregar as imagens da torre, da grade e da porta
-    torreImg = loadImage("torre.png");
-
-
-    //carregando as animações do fantasma
-    player_parado = loadAnimation("fantasma parado.png");
-    player_pulando = loadAnimation("fantasma pulando.png");
-
-    //carregar som
-
-
-
-    //criar os grupos das grades e das portas
-    grupoBlocoInvisivel = new Group();
+    
 
 
 
@@ -29,59 +16,14 @@ function preload() {
 
 function setup() {
     createCanvas(600, 600);
-    //criando a torre
-    torre = createSprite(300, 300);
-    torre.addImage("torre", torreImg);
-    torre.velocityY = 1;
-
-    //tocar o som em loop
-
-    //criando o player
-    player = createSprite(200, 200, 50, 50);
-    player.addAnimation("player parado", player_parado);
-    player.addAnimation("player pulando", player_pulando)
-    player.scale = 0.3;
-
-    edges = createEdgeSprites();
+ 
 }
 
 function draw() {
     background(200);
 
     if (estadoJogo === "jogar") {
-        player.changeAnimation("player parado");
-
-        //código para controlar o jogador para cima
-        if (keyDown("space")) {
-            player.velocityY = -10;
-            player.changeAnimation("player pulando");
-        }
-
-        //código para controlar o jogador para Esquerda
-        if (keyDown("space")) {
-            player.x -= 3;
-            player.changeAnimation("player pulando");
-        }
-
-        //código para controlar o jogador para direita
-
-
-        //gravidade
-        player.velocityY += 0.8;
-
-        //Código para reiniciar a torre
-
-
-        //código para descansar na torre
-
-
-        gerarPortas();
-        drawSprites();
-
-        //código para finalizar o jogo
-        if (player.isTouching(grupoBlocoInvisivel) || player.y > height) {
-            estadoJogo = "fim";
-        }
+     
     }
     if (estadoJogo === "fim") {
         background(0)
@@ -92,38 +34,3 @@ function draw() {
 
 }
 
-
-function gerarPortas() {
-
-    if (frameCount % 240 === 0) {
-
-        porta = createSprite(200, -50);
-        porta.x = Math.round(random(120, 400))
-
-        grade = createSprite(porta.x, 10);
-
-        blocoInvisivel = createSprite(porta.x, 15, grade.width, 2);
-
-        //fazer o bloco invisivel ser invisivel
-
-
-        //código para adicionar as imagens nas sprites
-
-
-        //velocidade das sprites
-        blocoInvisivel.velocityY = 1;
-
-
-        //tempo de vida das sprites
-        blocoInvisivel.lifetime = 800;
-
-
-        //adicionar nos grupos as sprites
-        grupoBlocoInvisivel.add(blocoInvisivel);
-
-
-
-    }
-
-
-}
